a4e54e2164edb52bbeac8df964853b01f935d035
24LP8740 24LP8740 <24LP8740@administrators-iMac-2.local>
2025-02-13 16:41:35 +0800

    done

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,19 @@ import { Skill } from "../components/Skills";
 import { Experience } from "../components/Experience";
 import { Work } from "../components/work";
 import { Footer } from "../components/footer";
+import Styles from "../styles/Display.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Skill />
-      <Experience />
-      <Work />
-      <Footer />
+    <div className={Styles.display}>
+      <div className={Styles.setdisplay}>
+        <Navbar />
+        <Hero />
+        <Skill />
+        <Experience />
+        <Work />
+        <Footer />
+      </div>
     </div>
   );
 }
